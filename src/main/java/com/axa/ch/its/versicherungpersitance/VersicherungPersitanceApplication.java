baf350d16cbbc16dbef9e64429f73b9c93c0e4c4
Maere77 <74065531+Maere77@users.noreply.github.com>
2024-07-11 10:51:55 +0200

    first commit

---
+++ src/main/java/com/axa/ch/its/versicherungpersitance/VersicherungPersitanceApplication.java
@@ -0,0 +1,13 @@
+package com.axa.ch.its.versicherungpersitance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersicherungPersitanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersicherungPersitanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axa/ch/its/versicherungpersitance/VersicherungPersitanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axa.ch.its.versicherungpersitance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersicherungPersitanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
