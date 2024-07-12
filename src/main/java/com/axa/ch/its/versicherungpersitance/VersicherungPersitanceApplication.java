85f6eff189497ea244f3a94565f54052d9e36df0
Maere77 <74065531+Maere77@users.noreply.github.com>
2024-07-12 11:13:17 +0200

    third commit

--- src/main/java/com/axa/ch/its/versicherungpersitance/VersicherungPersitanceApplication.java
+++ src/main/java/com/axa/ch/its/versicherungpersitance/VersicherungPersitanceApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "My API", version = "1.0", description = "My API for managing contracts"))
+@OpenAPIDefinition(info = @Info(title = "My API ", version = "1.0", description = "My API for managing contracts"))
 @SpringBootApplication
 public class VersicherungPersitanceApplication {
 
